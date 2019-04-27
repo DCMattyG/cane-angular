3a1b24236716527d6ee3a4dac83b6da528c87331
dcmattyg <mgarrett0402@gmail.com>
2019-04-27 16:57:11 -0700

    Added DEV environment config file for testing.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // baseUrl: 'http://cane.cisco.com'
-  baseUrl: 'http://127.0.0.1:8005'
+  baseUrl: 'http://cane.cisco.com'
+  // baseUrl: 'http://127.0.0.1:8005'
 };
